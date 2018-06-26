c05406021377aeb2d71071f5b130044238fec18f
SaitouRedEyes <zeca.saitou@gmail.com>
2018-06-26 15:02:51 -0300

    Xamarin Broadcast Example
    This example show the Broadcast use on Xamarin Android.

---
+++ BroadcastSample/BroadcastSample/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Content;
+
+namespace BroadcastSample
+{
+    [Activity(Label = "BroadcastSample", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button buttonSendBroadcast = FindViewById<Button>(Resource.Id.sendBroadcast);
+
+            buttonSendBroadcast.Click += OnButtonSendBroadcastClicked;
+        }
+
+        private void OnButtonSendBroadcastClicked(object sender, EventArgs e)
+        {
+            Intent i = new Intent("MyBroadcastReceiver");
+
+            Bundle myParameters = new Bundle();
+            myParameters.PutString("App", "BroadcastSample");
+
+            i.PutExtras(myParameters);
+            SendBroadcast(i);
+        }
+    }
+}
+
---
+++ BroadcastSample/BroadcastSample/MyBroadcastReceiver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BroadcastSample
+{
+    [BroadcastReceiver]
+    [IntentFilter(new[] { "MyBroadcastReceiver" }, Categories = new[] { Intent.CategoryDefault })]
+    public class MyBroadcastReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            if (intent != null)
+            {
+                Bundle myParameters = intent.Extras;
+
+                if (myParameters != null)
+                {
+                    Toast.MakeText(context, "Received from: " + myParameters.GetString("App"), ToastLength.Short).Show();
+                }
+            }
+            else
+            {
+                Toast.MakeText(context, "Received from some app:", ToastLength.Short).Show();
+            }
+        }
+    }
+}
